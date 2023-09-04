9160696ea40365b7d7efce6ee3ecedea38a27019
flogertciku <floggertciku@gmail.com>
2023-09-04 18:01:53 +0200

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import NavBar from './components/NavBar'
 import Form from './components/Form'
 import List from './components/List'
 import Details from './components/Details'
+import LearningApi from './components/LearningApi'
 function App() {
   const [list, setList] = useState([])
   const [userForm,setUserForm] = useState({})
 
   return (
     <div className='row'>
-    <NavBar pageNumber= {4} pageTitle= "HomePAge"></NavBar>
+    {/* <NavBar pageNumber= {4} pageTitle= "HomePAge"></NavBar>
     <Form  list = {list} setList= {setList} setUserForm= {setUserForm}></Form>
     <List list= {list}></List>
     <Details userName ={ userForm.username} email={userForm.email} password= {userForm.password}></Details>
-   
+    */}
+  <LearningApi></LearningApi>
+
+
     </div>
   )
 }
